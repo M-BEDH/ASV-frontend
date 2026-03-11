4fb14931b65ae53b9a3e03dd6c21cca1795190ed
M-BEDH <https://github.com/M-BEDH>
2026-03-11 17:58:09 +0100

    retrait import font.css

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,14 @@
 import { Stack } from 'expo-router';
 import { AuthProvider } from '../context/AuthContext';
-import '../styles/index.css';
-import '../styles/font.css';
+import { ThemeProvider } from '../context/ThemeContext';
+
 
 export default function RootLayout() {
   return (
-    <AuthProvider>
-      <Stack screenOptions={{ headerShown: false }} />
-    </AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <Stack screenOptions={{ headerShown: false }} />
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
