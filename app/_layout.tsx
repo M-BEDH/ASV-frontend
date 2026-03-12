b2e2bab801da72f8a9f0f42d5baed110590fb1f9
M-BEDH <https://github.com/M-BEDH>
2026-03-12 23:27:44 +0100

    add toast info

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,15 @@
 import { Stack } from 'expo-router';
 import { AuthProvider } from '../context/AuthContext';
 import { ThemeProvider } from '../context/ThemeContext';
-
+import { ToastProvider } from '../context/ToastContext';
 
 export default function RootLayout() {
   return (
     <ThemeProvider>
       <AuthProvider>
-        <Stack screenOptions={{ headerShown: false }} />
+        <ToastProvider>
+          <Stack screenOptions={{ headerShown: false }} />
+        </ToastProvider>
       </AuthProvider>
     </ThemeProvider>
   );
