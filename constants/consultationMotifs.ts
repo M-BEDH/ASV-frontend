ed439646cd425ed10f33c27a5abbdb82827bd7f1
M-BEDH <mel.b.ms@pm.me>
2026-04-03 09:31:09 +0200

    motif consule externalisé

---
+++ constants/consultationMotifs.ts
@@ -0,0 +1,6 @@
+export const consultationMotifs = [
+  { label: 'Vaccin', value: 'Vaccin' },
+  { label: 'Consultation', value: 'Consultation' },
+  { label: 'Urgence', value: 'Urgence' },
+  { label: 'Autre', value: 'Autre' },
+];
